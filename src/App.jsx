b4f9e7ab7cf549c4bab3de78071be70230a8dca6
ncperes <nperes@psgtecnologia.com.br>
2022-08-24 12:27:40 -0400

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
-    // throw new Error('Eita Giovana, o forninho caiu!');
-
-    return <h1>Hello JS World</h1>
+    return (
+        <>
+            <RepositoryList />
+        </>
+        
+    )
 }
